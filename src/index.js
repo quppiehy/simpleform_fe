174d86067dbef49dd0d83b10407590b5c54e2652
quppiehy <127730321+quppiehy@users.noreply.github.com>
2024-03-06 22:18:39 +0800

    Removed extra comments

--- src/index.js
+++ src/index.js
@@ -3,20 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// function tick() {
-//   const element = (
-//     <div>
-//       <h1>Hello, world!</h1>
-//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
-//     </div>
-//   );
-//   root.render(element);
-// }
-
-// setInterval(tick, 1000);
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>

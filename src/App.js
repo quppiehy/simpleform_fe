ccd118019d907e055fd101eff51dc51cb3ce5234
quppiehy <sunhuiyi@gmail.com>
2024-03-06 12:17:52 +0800

    simple form front end created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Swal from "sweetalert2";
+import * as SwalMsgs from "./Utils/SwalMsgs";
+import axios from "axios";
 
 function App() {
+  const [fieldValues, setFieldValues] = useState({});
+  const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
+  const handleChange = (fieldName, value) => {
+    setFieldValues((prevValues) => ({
+      ...prevValues,
+      [fieldName]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newFieldErrors = {};
+    Object.keys(fieldValues).forEach((fieldName) => {
+      if (typeof fieldValues[fieldName] === "string") {
+        if (fieldValues[fieldName].trim() === "") {
+          newFieldErrors[fieldName] = true;
+        }
+      }
+    });
+
+    if (newFieldErrors && newFieldErrors.length > 0) {
+      Swal.fire(SwalMsgs.missingFormInfoGentle);
+    } else {
+      const newEntry = { fieldValues: fieldValues };
+
+      try {
+        const addedEntry = await axios.put(
+          `${BACKEND_URL}/submit/`,
+          newEntry
+          // {
+          //   headers: {
+          //     Authorization: `Bearer ${currUser.accessToken}`,
+          //   },
+          // }
+        );
+        const newObject = addedEntry.data;
+
+        console.log("Added Entry: ", newObject);
+
+        Swal.fire(SwalMsgs.successPosting);
+      } catch (error) {
+        Swal.fire(SwalMsgs.errorPosting);
+      }
+    }
+  };
+
+  useEffect(() => {
+    console.log("Name: ", fieldValues.name);
+  }, [fieldValues.name]);
+
+  useEffect(() => {
+    console.log("Message :", fieldValues.message);
+  }, [fieldValues.message]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h2>Simple Form</h2>
+          <p>Please enter your name, message and submit.</p>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>
+              Input Name:{" "}
+              <input
+                type="text"
+                value={fieldValues.name}
+                onChange={(e) => handleChange("name", e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Input Message:{" "}
+              <input
+                type="message"
+                value={fieldValues.message}
+                onChange={(e) => handleChange("message", e.target.value)}
+              />
+            </label>
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
       </header>
     </div>
   );
---
+++ src/Utils/SwalMsgs.js
@@ -0,0 +1,17 @@
+export const successPosting = {
+  title: "Success!",
+  text: "Your post has been successfully submitted!",
+  icon: "success",
+};
+
+export const missingFormInfoGentle = {
+  title: "Ooops!",
+  text: "You need to fill up the required fields!",
+  icon: "error",
+};
+
+export const errorPosting = {
+  title: "Error posting!",
+  text: "Sorry, an error occured. Please resubmit your post.",
+  icon: "error",
+};
